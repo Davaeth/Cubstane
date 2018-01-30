a9567035b67f367ce98b6d5c6093973c1dfb4c55
Davaeth <masterbelki@gmail.com>
2018-01-30 22:41:50 +0100

    Adding new architecture (my own, I'm still trying to find best one for me) for project and some models for my school project.

---
+++ Assets/Scripts/Game_Mechanics/EndTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour {
+
+    public Level end;
+
+    void OnTriggerEnter()
+    {
+        end.CompleteLevel();
+        Invoke("LoadNextLevel", 1.7f);
+    }
+
+    void LoadNextLevel()
+    {
+        GameManager.LoadLevel();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+    public Level game;
+
+    public GameObject death;
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            Instantiate(death, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(gameObject);
+            FindObjectOfType<Level>().EndGame();
+        }
+	}
+}
